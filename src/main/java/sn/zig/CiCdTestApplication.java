6b8012f9447c61a4ade86a543639d742dd3d3ed9
BALLA <ballagningue2@gmail.com>
2023-03-17 15:54:20 -0100

    first commit

---
+++ src/main/java/sn/zig/CiCdTestApplication.java
@@ -0,0 +1,24 @@
+package sn.zig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class CiCdTestApplication {
+	
+	@GetMapping("/bonjour")
+	public String bonjour() {
+		return "Hello ___";
+	}
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdTestApplication.class, args);
+	}
+
+}
